63af71d415d9c6816f6f9e697e45fbe1aec70f67
pix-panther24 <154585738+pix-panther24@users.noreply.github.com>
2024-04-22 19:20:14 +0530

    Add files via upload

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      <p>Get me A Coffee - Fund your projects with coffee</p>
+    </footer>
+  )
+}
+
+export default footer
